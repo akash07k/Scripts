4bbeea1b5167dd5c1383e9fd1dbff8130252225b
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-07-02 01:06:33 +0530

    LaunchAndroidBuildEnv: Use formatted strings for the arguments, reformat the code and use multiline strings for the arguments

--- LaunchAndroidBuildEnv.py
+++ LaunchAndroidBuildEnv.py
@@ -3,8 +3,9 @@ import sys
 
 print("---Android build environment launcher---")
 
+
 def elevate():
-    print("Checking if running with administrator priveleges...");
+    print("Checking if running with administrator priveleges...")
     if ctypes.windll.shell32.IsUserAnAdmin():
         # Already running with administrator privileges
         print("Already running with administrator priveleges...")
@@ -13,16 +14,17 @@ def elevate():
     print("Not running with administrator priveleges...")
     # Re-launch cmd.exe with administrator privileges and pass the arguments
     print("Re-launching the command prompt with administrator priveleges and passing the arguments to it...")
-    terminal_args = "new-tab --title \"AndroidBuildEnv\" cmd /k "
-    command_args = 'wsl --shutdown && wsl --mount \\\\.\\PHYSICALDRIVE3 --partition 2 --name rom && wsl'
+    terminal_args = """new-tab --title "AndroidBuildEnv" cmd /k """
+    command_args = """wsl --shutdown && wsl --mount \\\\.\\PHYSICALDRIVE3 --partition 2 --name rom && wsl"""
     ctypes.windll.shell32.ShellExecuteW(
         None,
         "runas",
         "wt.exe",
-         terminal_args + command_args,
+        f"{terminal_args}{command_args}",
         None,
         1
     )
+
 
 # Call the elevate function to launch cmd.exe as elevated with arguments
 elevate()
