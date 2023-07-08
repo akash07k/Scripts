dabdae7bb9de30d911cc3824afd21f3247efef0c
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-07-09 01:20:51 +0530

    AndroidConfInit: Add the support for multiple Telegram configs

--- AndroidConfInit.py
+++ AndroidConfInit.py
@@ -67,15 +67,40 @@ if choice == 1:
         rom_section: str = f"ROM{i}"
         config[rom_section] = rom
 if choice == 2:
-    telegram_section: str = "TELEGRAM"
-    config[telegram_section] = {}
-    config[telegram_section]["TELEGRAM_CONFIG_NAME"] = input(
-        "Telegram config name: (or press enter for Akash)") or "Akash"
-    config[telegram_section]["TELEGRAM_BOT_TOKEN"] = input(
-        "Telegram bot Token: ")
-    config[telegram_section]["TELEGRAM_CHAT_ID"] = input("Telegram chat ID: ")
-    config[telegram_section]["TELEGRAM_USER_NAME"] = input(
-        "Telegram user Name: (or press enter for @akashk07)") or "@akashk07"
+    telegram_configs: List[Dict[str, str]] = []
+    add_telegram_config: bool = True
+    while add_telegram_config:
+        telegram_config: Dict[str, str] = {}
+        telegram_config["TELEGRAM_CONFIG_NAME"] = input(
+            "Telegram config name: (or press enter for Akash)") or "Akash"
+        telegram_config["TELEGRAM_BOT_TOKEN"] = input(
+            "Telegram bot Token: ")
+        telegram_config["TELEGRAM_CHAT_ID"] = input(
+            "Telegram chat ID: ")
+        telegram_config["TELEGRAM_USER_NAME"] = input(
+            "Telegram user Name: (or press enter for @akashk07)") or "@akashk07"
+        telegram_configs.append(telegram_config)
+        print("Do you want to add another Telegram config?")
+        print("1. Yes")
+        print("2. No")
+        valid_choices: List[int] = [1, 2]
+        choice: int = 0
+        while choice not in valid_choices:
+            try:
+                choice = int(input("Enter your choice: "))
+                if choice not in valid_choices:
+                    print("Invalid choice. Please enter a valid option.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+            if choice != 1:
+                add_telegram_config = False
+                break
+            else:
+                add_telegram_config = True
+    for i, telegram_config in enumerate(telegram_configs, start=1):
+        telegram_section: str = f"TELEGRAM{i}"
+        config[telegram_section] = telegram_config
+
 
 with open('config.ini', 'a') as configfile:  # Use 'a' mode to append
     config.write(configfile)
