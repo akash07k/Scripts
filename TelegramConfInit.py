7828a3dcc480f896c9506c814142b248434eb58c
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-07-23 14:46:56 +0530

    TelegramConfInit: Rewrite from the scratch

--- TelegramConfInit.py
+++ TelegramConfInit.py
@@ -1,70 +1,60 @@
-from typing import List, Dict
+import click
 import configparser
 import os
 
 
-def add_telegram_config():
-    telegram_configs: List[Dict[str, str]] = []
-    add_telegram_config: bool = True
-    while add_telegram_config:
-        telegram_config: Dict[str, str] = {}
-        telegram_config["TELEGRAM_CONFIG_NAME"] = input(
-            "Telegram config name: (or press enter for Akash)") or "Akash"
-        telegram_config["TELEGRAM_BOT_TOKEN"] = input(
-            "Telegram bot Token: ")
-        telegram_config["TELEGRAM_CHAT_ID"] = input(
-            "Telegram chat ID: ")
-        telegram_config["TELEGRAM_USER_NAME"] = input(
-            "Telegram user Name: (or press enter for @akashk07)") or "@akashk07"
-        telegram_configs.append(telegram_config)
-        print("Do you want to add another Telegram config?")
-        print("1. Yes")
-        print("2. No")
-        valid_choices: List[int] = [1, 2]
-        choice: int = 0
-        while choice not in valid_choices:
-            try:
-                choice = int(input("Enter your choice: "))
-                if choice not in valid_choices:
-                    print("Invalid choice. Please enter a valid option.")
-            except ValueError:
-                print("Invalid input. Please enter a number.")
-            if choice != 1:
-                add_telegram_config = False
-                break
-            else:
-                add_telegram_config = True
-        for i, telegram_config in enumerate(telegram_configs, start=1):
-            telegram_section: str = f"TELEGRAM{i}"
-            config[telegram_section] = telegram_config
+def main():
+    click.echo("Welcome to telegram configuration initializer")
+    click.echo("Written by Akash Kakkar")
+    add_config()
 
 
 config = configparser.ConfigParser()
-print("Welcome to Configuration Initializer")
-print("By Akash Kakkar")
-print("What do you want to do?")
-print("1. Add telegram config")
-
-valid_choices: List[int] = [1,]
-choice: int = 0
-
-while choice not in valid_choices:
-    try:
-        choice = int(input("Enter your choice: "))
-        if choice not in valid_choices:
-            print("Invalid choice. Please enter a valid option.")
-    except ValueError:
-        print("Invalid input. Please enter a number.")
-if choice == 1:
-    add_telegram_config()
+CONFIG_DIR = "configs"
+CONFIG_FILE = f"{CONFIG_DIR}/telegram.ini"
 
 
 def write_configuration():
     # Check if "configs" directory exists, and create it if it doesn't
-    if not os.path.exists("configs"):
-        os.makedirs("configs")
+    if not os.path.exists(CONFIG_DIR):
+        os.makedirs(CONFIG_DIR)
 
     # Write or append the configuration file
-    with open("configs/telegram.ini", "a") as configfile:
+    with open(CONFIG_FILE, "a") as configfile:
         config.write(configfile)
-    print("Configuration file created/appended successfully")
+    click.echo("Configuration file created/appended successfully")
+
+
+def is_section_duplicate(section_name: str) -> bool:
+    """Check if the section already exists in the configuration file."""
+    config.read(CONFIG_FILE)
+    return section_name in config.sections()
+
+
+@click.command()
+@click.option("--name", prompt="Enter your Telegram config name",
+              help="Enter your Telegram config name", default="default")
+@click.option("--token", prompt="Enter your Telegram bot token",
+              help="Enter your Telegram bot token", default="123456789:abcdefghijklmnopqrstuvwxyz")
+@click.option("--chatid", prompt="Enter your Telegram chat ID",
+              help="Enter your Telegram chat ID", default="123456789")
+@click.option("--username", prompt="Enter your Telegram username",
+              help="Enter your Telegram username", default="akashk07")
+def add_config(name: str, token: str, chatid: str, username: str):
+    """Add a new configuration to the configuration file"""
+    if is_section_duplicate(f"{name}_Telegram"):
+        click.echo(
+            f"Configuration section '{name}_Telegram' already exists. Please choose a different name.")
+        return
+
+    config[f"{name}_Telegram"] = {
+        "name": name,
+        "token": token,
+        "chatid": chatid,
+        "username": username
+    }
+    write_configuration()
+
+
+if __name__ == "__main__":
+    main()
